e2a794cd33dad74eebbeb5e6fee6a8de3e0b7249
Andrea <andrea.mazzeschi@gmail.com>
2012-01-19 18:27:44 +0100

    a

---
+++ test.cc
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  19/01/2012 19:21:28
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Ciao" ;
+	cout << "Stronzo"<<endl;
+	return 0;
+}
