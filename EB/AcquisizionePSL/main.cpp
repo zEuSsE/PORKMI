b3f2606b357f041d0b70dd3df0bd80095721c41e
Andrea <andrea.zerbini8@gmail.com>
2012-01-23 20:58:41 +0100

    Dovrebbe fungere con libreria ora

--- EB/AcquisizionePSL/main.cpp
+++ EB/AcquisizionePSL/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "point_set/point.h"
 
+
+using namespace libpmk;
 using namespace std;
+
 
 int main()
 {
+    Point punto(14);
+
     cout << "Hello world!" << endl;
     return 0;
 }
