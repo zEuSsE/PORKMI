7c4c2e54cbdb990748fc4ee0143df79079f08449
Andrea <andrea.zerbini8@gmail.com>
2012-01-24 19:03:35 +0100

    Si passa a Eclisse

--- EB/AcquisizionePSL/main.cpp
+++ EB/AcquisizionePSL/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "point_set/point.h"
-
+#include <point_set/point.h>
+#include <point_set/on-disk-point-set-list.h>
 
 using namespace libpmk;
 using namespace std;
@@ -8,6 +8,11 @@ using namespace std;
 
 int main()
 {
+
+
+    PointSetList *	pointSetList=new OnDiskPointSetList("~/Scrivania/PORKMI/libpmk-2.5/libpmk2/pyramids/testdata/testpointsets.psl");
+	cout << pointSetList;
+
     Point punto(14);
 
     cout << "Hello world!" << endl;
