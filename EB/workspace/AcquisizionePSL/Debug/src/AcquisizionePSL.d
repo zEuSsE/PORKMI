a97ef3f158791d325a30b5ec67ce7ffec59be0cd
Andrea <andrea.zerbini8@gmail.com>
2012-01-27 18:48:01 +0100

    Aggiornamento con LabeledIndex da finire

--- EB/workspace/AcquisizionePSL/Debug/src/AcquisizionePSL.d
+++ EB/workspace/AcquisizionePSL/Debug/src/AcquisizionePSL.d
@@ -1,9 +1,12 @@
 src/AcquisizionePSL.d: ../src/AcquisizionePSL.cpp \
+ /home/andrea/workspace/libpmk-2.5/libpmk2/util/labeled-index.h \
  /home/andrea/workspace/libpmk-2.5/libpmk2/point_set/point-set-list.h \
  /home/andrea/workspace/libpmk-2.5/libpmk2/point_set/point-set.h \
  /home/andrea/workspace/libpmk-2.5/libpmk2/point_set/point.h \
  /home/andrea/workspace/libpmk-2.5/libpmk2/point_set/point-ref.h \
  /home/andrea/workspace/libpmk-2.5/libpmk2/point_set/mutable-point-set-list.h
+
+/home/andrea/workspace/libpmk-2.5/libpmk2/util/labeled-index.h:
 
 /home/andrea/workspace/libpmk-2.5/libpmk2/point_set/point-set-list.h:
 
