6d5463f97eeedcb12f58b22a231a7f876f48ccd1
zEuSsE <andrea.mazzeschi@gmail.com>
2012-01-30 10:50:02 +0100

    30/01

--- zEuSsE/ML3/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lpmk -lboost_system -lpmk_util -lstdc++ -lboost_filesystem
-
